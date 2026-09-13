689322e80d4ebb959137319853662143a77e77f2
Jimmy Berríos Gallo (Jimmy Dev & Geek) <41551757+JBerriosG@users.noreply.github.com>
2026-09-13 18:40:51 +0000

    nuevo adapter de vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 
 import node from '@astrojs/node';
+import vercelAdapter from '@astrojs/vercel';
 
 // https://astro.build/config
 export default defineConfig({
@@ -12,7 +13,5 @@ export default defineConfig({
     plugins: [tailwindcss()]
   },
 
-  adapter: node({
-    mode: 'standalone'
-  })
+  adapter: vercelAdapter()
 });
